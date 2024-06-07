b07e8eb2878b2e4ffd04df1c08b60bedae500077
Prithviraj Reddy <71643835+PrithviRajReddy24@users.noreply.github.com>
2024-06-07 12:05:28 +0530

    Create test.py - added a new test python file to the repo

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import keras as ks
+import tensorflow
+% mtplotlib inline
+import warnings
+warnings.ignore("warnings")
+import metrics
