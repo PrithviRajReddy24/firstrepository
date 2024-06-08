4c331275b5b8c3e987a377f66c8bd237a9e19566
unknown <velma.pruthvi24@gmail.com>
2024-06-08 19:32:11 +0530

    Made further changes in test.py file for testing in jenkins pipeline

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import keras as ks
-import tensorflow
+#import pandas as pd
+#import numpy as np
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+#import keras as ks
+#import tensorflow
+
+print("Hello World!")
